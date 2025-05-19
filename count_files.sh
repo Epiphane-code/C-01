e9515ce9e679894a21a5b928e0bc44f8ead9726b
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-19 19:00:19 +0200

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -2,7 +2,5 @@
 #demander d'entrer le chemin du repertoire
 read myfolder
 #compter le nombres de fichiers dans le repertoire
-file_count_calc=$(ls "$myfolder"|wc -l)
-num_files=$(echo "$file_count_calc")
-echo ""
-echo "Le dossier $myfolder contient $num_files fichier(s)."
+file_count_calc=$(ls "$myfolder" | wc -l)
+echo "Le dossier $myfolder contient $file_cont_calc fichier(s)."
