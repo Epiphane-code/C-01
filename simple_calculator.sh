337fdd22a0eb2d92d4e95ed235a4ba481408eb93
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-19 18:54:16 +0200

    Update simple_calculator.sh

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,33 +1,27 @@
 #!/bin/bash
 # simple calculatrice
 
-addition="+"
-soustraction="-"
-multiplication="*"
-division="/"
-modulo="%"
-
 read a b op
 
 case $op in
-$addition)
+"+")
 echo "Résultat : $(a + b)"
 ;;
-$soustraction)
+"-")
 echo "Résultat : $(a - b)"
 ;;
-$multiplication)
+"*")
 echo "Résultat : $(a * b)"
 ;;
-$division)
+"/")
 if [ b != 0 ]; then
 echo "Résultat : $(a / b)"
 fi
 ;;
-$modulo)
+"%")
 echo "Résultat : $(a % b)"
 ;;
 *)
-echo "Résultat : 0"
+exit 0
 ;;
 esac
