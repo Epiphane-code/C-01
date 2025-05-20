56bcf76d9b51a1dd661fa708ee4bd42b5ffdfa89
Epiphane Nicoue Omar <omarepiphanefarouk@gmail.com>
2025-05-20 12:02:09 +0200

    Delete count_files.sh

--- count_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#demander d'entrer le chemin du repertoire
-read myfolder
-#compter le nombres de fichiers dans le repertoire
-file_count_calc=$(ls "$myfolder" | wc -l)
-echo "Le dossier $myfolder contient $file_cont_calc fichier(s)."
